14eb35f5a28baafd68d74c7214af7781c4bd9a29
Adrian Boncodin <juelboncodin@gmail.com>
2025-07-23 15:30:44 +0800

    Fixed export statement issue at tailwind.config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+
 
 const Header = () => {
   return (
     <header className="absolute w-full z-50 transition-all duration-300">
-
-        <div className='constainer mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between'>
-
-            {/* Logo Name */}
-            <div className='flex items-center'>Adrian Boncodin</div>
+      <div className='container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between'>
+        {/* Logo Name */}
+        <div className='flex items-center'>
+          <div className='h-10 w-10 rounded-xl bg-gradient-to-r from-gray-500 to-gray-100 flex items-center justify-center text-purple-600
+          font-bold text-xl mr-3'>A</div>
         </div>
+      </div>
     </header>
   )
 }
