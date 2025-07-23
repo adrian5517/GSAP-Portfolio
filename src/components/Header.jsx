6863c0ed15f0f7b75a8ff7cbfb595c390d3e14f8
adrian5517 <juelboncodin@gmail.com>
2025-07-23 18:10:40 +0800

    Added Desktop Navigation with framer motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import {motion} from 'framer-motion';
+import { a } from 'framer-motion/client';
 
 const Header = () => {
   return (
@@ -19,14 +20,42 @@ const Header = () => {
         className='flex items-center'>
 
           <div className='h-10 w-10 rounded-xl bg-gradient-to-r from-gray-500 to-gray-100 flex items-center justify-center text-purple-600
-          font-bold text-xl mr-3'>A</div>
+          font-bold text-xl mr-3'>AB</div>
 
           <span className="font-poppins text-white text-xl font-bold bg-gradient-to-r from gray-300 to-gray-100
           bg-clip-text text-transparent">AdrianBoncodin</span>
 
             
         </motion.div>
+        {/* Navigation Links */}
+        <nav className='lg:flex hidden space-x-8'>
+        {["Home", "About", "Projects", "Contact"].map((item,index) =>(
+          <motion.a 
+          key={item}
+          initial={{ opacity: 0, y: -20}}
+          animate={{ opacity: 1 , y: 0}}
+          transition={{
+            type: 'spring',
+            stiffness:100,
+            damping: 20,
+            delay: 0.7 + index * 0.2,
+          }}
+          className='relative font-poppins text-gray-800 dark:text-gray-200 hover-violet-600 dark:hover:text-violet-400
+          font-medium transition-colors duration-300 group'
+          href='#'>{item}
+          <span className="absolute bottom-0 left-0 w-0 h-0.5
+          bg-violet-600 group-hover:w-full transition-all duration-300 "></span>
+          </motion.a>
+        ))}
+      </nav>
+
+
       </div>
+
+      
+      
+
+
     </header>
   )
 }
