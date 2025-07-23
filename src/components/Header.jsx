50fcdc52bee0c9b2f7716332f4864988caf4628b
adrian5517 <juelboncodin@gmail.com>
2025-07-23 18:45:39 +0800

    Added hire me button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -87,6 +87,11 @@ const Header = () => {
         </motion.a>
       </div>
 
+      {/* Hire Me Button */}
+      <button className='ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400
+      to-gray-200 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white
+      transition-all duration-500'>Hire Me</button>
+
 
       </div>
 
