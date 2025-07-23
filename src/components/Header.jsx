6cbe0573282861c0a91b0a2b08efedf1dc9f96a3
Adrian Boncodin <juelboncodin@gmail.com>
2025-07-23 15:15:51 +0800

    Starting for header styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="absolute w-full z-50 transition-all duration-300">
+
+        <div className='constainer mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between'>
+
+            {/* Logo Name */}
+            <div className='flex items-center'>Adrian Boncodin</div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
