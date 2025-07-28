08b753ee35a61135f4b573f3172a366241cca0ad
Adrian Boncodin <juelboncodin@gmail.com>
2025-07-28 18:40:14 +0800

    Starting adding content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <div className='@container mx-auto h-screen flex flex-col items-center justify-center text-center p-10'>
       <h1>About Me</h1>
       <p>
         I'm a MERN stack web and mobile developer focused on building responsive, user-friendly applications that solve real-world problems and support business growth. I specialize in both front-end and back-end development, delivering clean, maintainable, and high-quality solutions that meet client needs and exceed user expectations.
